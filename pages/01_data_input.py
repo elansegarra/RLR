7835702edeaa639b741ff2bfebdea1a3d0256e79
Elan Segarra <elanster@gmail.com>
2022-06-05 14:08:38 -0400

    Moved and connected var schema definition

--- pages/01_data_input.py
+++ pages/01_data_input.py
@@ -18,12 +18,23 @@ def remove_data_file(side):
     else:
         raise NotImplementedError(f"Unrecognized side, {side}, must by 'l' or 'r'.")
 
+def add_var_group():
+    # Add empty var group to end of list of the variable group schema
+    var_comp_schema = st.session_state['rlr'].get_var_comp_schema()
+    var_comp_schema.append({'name': "", 'lvars': [], 'rvars': []})
+    st.session_state['rlr'].set_var_comp_schema(var_comp_schema)
+
+def del_var_group(vargp_index):
+    # Delete var group at the passed index in the list of variable group schemas
+    var_comp_schema = st.session_state['rlr'].get_var_comp_schema()
+    del var_comp_schema[vargp_index]
+    st.session_state['rlr'].set_var_comp_schema(var_comp_schema)
 
 ###########################################################################
 #### App - Inputting Data Files ###########################################
 ###########################################################################
 
-st.title("Load and Initialize Data Files")
+st.header("Data Loading and Initialization")
 with st.expander("Left Data Set:", expanded = True):
     # Checking if left data has already been loaded
     if st.session_state['rlr'].dataL_loaded:
@@ -99,6 +110,46 @@ with st.expander("Right Data Set:", expanded = True):
                 st.session_state['data_R_loaded'] = False
             st.markdown(msg_text, unsafe_allow_html=True)
 
-link_pairs = [[2,'D'],[6,'C'],[4,'G']]
-if 'curr_link_pair' not in st.session_state:
-    st.session_state['curr_link_pair'] = 0
+###########################################################################
+#### App - Setting Up Variable Comparison Schema ##########################
+###########################################################################
+
+st.header("Variable Comparison Group Definitions")
+# Checking if both data sets have been properly loaded
+if (st.session_state['rlr'].dataL_loaded) and (st.session_state['rlr'].dataR_loaded):
+    # Input description
+    st.write("Use the below inputs to designate which variables from each "+
+                "data set should be compared to one another. For example, comparing "+
+                "variable 'name' in the left dataset with the variables 'first_name' "+
+                "and 'last_name' in the right data set. Add or delete "+
+                "comparison groups as necessary.")
+
+    # Gather var schema from rlr instance if loaded
+    if (st.session_state['rlr'].var_schema_loaded):
+        var_comp_schema = st.session_state['rlr'].get_var_comp_schema()
+    else:
+        var_comp_schema = [{'name': "", 'lvars': [], 'rvars': []}]
+
+    # Gathering variables from loaded dataset
+    l_vars = st.session_state['rlr'].dataL.columns
+    r_vars = st.session_state['rlr'].dataR.columns
+
+    # Iterate through each var group and print out related inputs
+    for i in range(len(var_comp_schema)):
+        var_group = var_comp_schema[i].copy()
+        with st.expander(f"Var. Group: {var_group['name']}", expanded = var_group['name']==""):
+            col_1, col_2 = st.columns([1,3])
+            var_name = col_1.text_input("Var. Group Name:", value = var_group['name'], key = f"vargp_{i}_name")
+            vars_l = col_2.multiselect("Pick Vars from Left Data Set:", l_vars,
+                                        default = var_group['lvars'], key = f"vargp_{i}_lvars")
+            vars_r = col_2.multiselect("Pick Vars from Right Data Set:", r_vars, 
+                                        default = var_group['rvars'], key = f"vargp_{i}_rvars")
+            del_button = col_1.button("Delete Group", key = f"vargp_{i}_del",
+                                        on_click = del_var_group, args=(i, ))
+            var_comp_schema[i] = {'name': var_name, 'lvars': vars_l, 'rvars': vars_r}
+    st.session_state['rlr'].set_var_comp_schema(var_comp_schema)
+    add_button = st.button("Add Group", on_click = add_var_group)
+
+    # st.write(st.session_state['rlr'].get_var_comp_schema())
+else:
+    st.write("User must load two data sets with identifying variables before defining the comparison schema.")
